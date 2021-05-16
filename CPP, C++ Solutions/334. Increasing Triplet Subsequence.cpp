1acdd4117b957daa2334045213a8820729eff13c
arpitkekri <jainarpitkekri@gmail.com>
2021-05-16 21:40:58 +0530

    class Solution {

---
+++ CPP, C++ Solutions/334. Increasing Triplet Subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size();
+        int smaller = INT_MAX, next_smaller = INT_MAX;
+        for(int num: nums) {
+            if(num <= smaller) smaller = num;
+            else if(num <= next_smaller) next_smaller = num;
+            else return true;
+        }
+        return false;
+    }
+};
