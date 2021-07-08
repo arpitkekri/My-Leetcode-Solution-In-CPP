ab8bb5f89295cd2dfa617b97283214808ae49302
arpitkekri <jainarpitkekri@gmail.com>
2021-07-08 22:41:49 +0530

    1528. Shuffle String

---
+++ CPP, C++ Solutions/1528. Shuffle String.cpp
@@ -0,0 +1,11 @@
+// TC - O(N)
+// SC - O(N)
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string res = s;
+        for(int i = 0; i < indices.size(); i++)
+            res[indices[i]] = s[i];
+        return res;
+    }
+};
