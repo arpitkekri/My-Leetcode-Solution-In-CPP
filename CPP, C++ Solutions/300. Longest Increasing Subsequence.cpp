7713db65058662bee169c76a7f157fab50dd46ad
arpitkekri <jainarpitkekri@gmail.com>
2021-07-09 14:11:50 +0530

    300. Longest Increasing Subsequence

--- CPP, C++ Solutions/300. Longest Increasing Subsequence.cpp
+++ CPP, C++ Solutions/300. Longest Increasing Subsequence.cpp
@@ -1,20 +1,33 @@
-// TC - O(N^2) 
+// TC - O(NlogN) 
 // SC - O(N)
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size(), res = 1;
-        vector<int> dp(n, 1);
+        int n = nums.size();
+        vector<int> dp;
+        dp.push_back(nums[0]);
         for(int i = 1; i < n; i++) {
-            // Compute dp[i]
-            for(int j = i-1; j >= 0; j--) {
-                if(nums[i] > nums[j]) 
-                    dp[i] = max(dp[i], 1 + dp[j]);
+            
+            if(dp.back() < nums[i]) 
+                dp.push_back(nums[i]);
+            
+            else {
+                /***************************
+                // This is also right but below code is more compact
+                int lo = 0, hi = dp.size(), mid;
+                hi = hi-1;
+                while(lo < hi) {
+                    mid = lo + (hi-lo)/2;
+                    if(dp[mid] >= nums[i])
+                        hi = mid;
+                    else lo = mid + 1;
+                }
+                dp[lo] = nums[i];
+                ******************************/
+                int idx = lower_bound(dp.begin(), dp.end(), nums[i]) - dp.begin();
+                dp[idx] = nums[i];
             }
-            res = max(res, dp[i]);
         }
-        return res;
+        return dp.size();
     }
 };
-
-// Note: O(NlogN) solution exist watch it on YT good/famous Intrerview problem
