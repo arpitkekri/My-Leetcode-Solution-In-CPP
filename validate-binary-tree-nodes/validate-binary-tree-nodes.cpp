b9251d5b94d2b3c23c0f04c6c8deae589e426608
Arpit Kumar Jain <46723473+arpitkekri@users.noreply.github.com>
2021-10-13 18:56:31 +0530

    Time: 36 ms (94.51%), Space: 31.9 MB (91.77%) - LeetHub

--- validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
+++ validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int find(int v, vector<int> &parent) {
-        if(parent[v] == -1) return v;
+        if(parent[v] == v) return v;
         return parent[v] = find(parent[v], parent);
     }
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-        vector<int> parent(n, -1);
+        vector<int> parent(n, 0);
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
         int p1, p2;
         for(int i = 0; i < n; i++) {
             p1 = find(i, parent);
@@ -24,7 +27,7 @@ public:
         }
         int count = 0;
         for(int i = 0; i < n; i++) {
-            if(parent[i] == -1) count++;
+            if(parent[i] == i) count++;
         }
         return count == 1;
     }
