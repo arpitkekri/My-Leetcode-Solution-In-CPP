028122de31cc73a79a11cde41e6568decc66bd27
Arpit Kumar Jain <46723473+arpitkekri@users.noreply.github.com>
2021-10-14 12:17:45 +0530

    Time: 200 ms (83.16%), Space: 126.4 MB (93.66%) - LeetHub

--- stone-game-iii/stone-game-iii.cpp
+++ stone-game-iii/stone-game-iii.cpp
@@ -27,8 +27,10 @@ public:
     }
 };
 */
+
 // Step - 2: Recursion + memoization (DP)
 // TC - O(N) + SC - O(N)
+/*
 class Solution {
 public:
     int helper(vector<int> &stoneValue, int start, vector<int> &dp) {
@@ -60,4 +62,60 @@ public:
         return "Bob";
     }
 };
+*/
 
+// Step - 3: Bottom Up DP
+// TC - O(N) + SC - O(N)
+/*
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<int> dp(n+1, INT_MIN);
+        dp[n] = 0;
+        
+        for(int i = n-1; i >= 0; i--) {
+            dp[i] = max(dp[i], stoneValue[i] - dp[i+1]);
+            
+            if(i + 1 < n)
+                dp[i] = max(dp[i], stoneValue[i] + stoneValue[i+1] - dp[i+2]);
+            
+            if(i + 2 < n)
+                dp[i] = max(dp[i], stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[i+3]);
+        }
+        
+        if(dp[0] > 0) return "Alice";
+        else if(dp[0] == 0) return "Tie";
+        return "Bob";
+    }
+};
+*/
+
+// Step - 4: Bottom Up DP
+// TC - O(N) + SC - O(1)
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<int> dp(3, INT_MIN);
+        dp[n%3] = 0;
+        
+        for(int i = n-1; i >= 0; i--) {
+            int ans = INT_MIN;
+            
+            ans = max(ans, stoneValue[i] - dp[(i+1)%3]);
+    
+            if(i + 1 < n)
+                ans = max(ans, stoneValue[i] + stoneValue[i+1] - dp[(i+2)%3]);
+            
+            if(i + 2 < n)
+                ans = max(ans, stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[i%3]);
+            
+            dp[i%3] = ans;
+        }
+        
+        if(dp[0] > 0) return "Alice";
+        else if(dp[0] == 0) return "Tie";
+        return "Bob";
+    }
+};
