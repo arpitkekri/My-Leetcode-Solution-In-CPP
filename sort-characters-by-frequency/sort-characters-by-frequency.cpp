bf5610ec0d245381c64d158b2adcc14474575e82
Arpit Kumar Jain <46723473+arpitkekri@users.noreply.github.com>
2021-10-22 13:24:58 +0530

    Time: 16 ms (43.52%), Space: 7.8 MB (99.80%) - LeetHub

--- sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<int> freq(256, 0);
+        unordered_map<char, int> freq;
         for(int i = 0; i < s.size(); i++) {
-            freq[(int)s[i]]++;
+            freq[s[i]]++;
         }
-        vector<pair<int, int>> freqSort;
-        for(int i = 0; i < 256; i++) {
-            if(freq[i] > 0) {
-                freqSort.push_back({freq[i], i});
-            }
+        vector<char> letter;
+        for(auto itr: freq) {
+            letter.push_back(itr.first);
         }
-        sort(freqSort.rbegin(), freqSort.rend());
+        sort(letter.begin(), letter.end(), [&](const char a, const char b){
+            return freq[a] > freq[b];
+        });
+        // vector<pair<int, int>> freqSort;
+        // for(int i = 0; i < 256; i++) {
+        //     if(freq[i] > 0) {
+        //         freqSort.push_back({freq[i], i});
+        //     }
+        // }
+        // sort(freqSort.rbegin(), freqSort.rend());
         s = "";
-        for(auto itr: freqSort) {
-            int f = itr.first;
-            char c = itr.second;
-            s.append(string(f, c));
+        for(auto itr: letter) {
+            int f = freq[itr];
+            int c = itr;
+            // char c = itr.second;
+            for(int i = 0; i < f; i++) 
+                s.push_back(c);
         }
         return s;
     }
