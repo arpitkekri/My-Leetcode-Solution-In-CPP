f75847ca450ff43396099763bb1b2c9922baa6a7
Arpit Kumar Jain <46723473+arpitkekri@users.noreply.github.com>
2021-10-29 01:45:45 +0530

    Time: 76 ms (78.77%), Space: 20 MB (67.11%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -3,24 +3,23 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        
         vector<vector<int>> triplets;
         int i = 0;
         while(i < n) {
+            int lo = i+1, hi = n-1;
             int a = nums[i];
-            int lo = i+1;
-            int hi = n-1;
-            int req = -a;
             while(lo < hi) {
-                if(nums[lo] + nums[hi] < req)
+                if(nums[lo] + nums[hi] < -a)
                     lo++;
-                else if(nums[lo] + nums[hi] > req)
+                else if(nums[lo] + nums[hi] > -a)
                     hi--;
                 else {
-                    int b = nums[lo], c = nums[hi];
-                    triplets.push_back({a, b, c});
-                    while(lo < n && nums[lo] == b) lo++;
-                    while(hi > i && nums[hi] == c) hi--;
+                    triplets.push_back({a, nums[lo], nums[hi]});
+                    
+                    while(lo < hi && nums[lo] == nums[lo+1]) lo++;
+                    while(lo < hi && nums[hi] == nums[hi-1]) hi--;
+                    
+                    lo++; hi--;
                 }
             }
             while(i < n && nums[i] == a) i++;
