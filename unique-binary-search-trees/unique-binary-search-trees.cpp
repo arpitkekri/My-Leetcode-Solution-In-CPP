5c3c1e55e60d3c226b7f4401fa6f160d0745f657
Arpit Kumar Jain <46723473+arpitkekri@users.noreply.github.com>
2021-11-08 09:41:36 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.18%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.cpp
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,13 +1,39 @@
+// class Solution {
+// public:
+//     int numTrees(int n) {
+//         vector<int> dp(n+1, 0);
+//         dp[0] = 1, dp[1] = 1;
+//         for(int i = 2; i <= n; i++) {
+//             for(int j = 0; j < i; j++) {
+//                 dp[i] += dp[j]*dp[i-j-1];
+//             }
+//         }
+//         return dp[n];
+//     }
+// };
+
+
+
+// USING catlan number Cn+1/Cn = 2*(2*n+1)/n+2;
+/*class Solution {
+public:
+    int numTrees(int n) {
+        vector<long> dp(2, 0);
+        dp[0] = 1, dp[1] = 1;
+        for(int i = 1; i < n; i++) {
+            dp[(i+1)%2] = dp[i%2]*2*(2*i+1)/(i+2);
+        }
+        return dp[n%2];
+    }
+};*/
+
 class Solution {
 public:
     int numTrees(int n) {
-        // catalan number
-        // c0*c(n-1)  + c1*cn-2 + ... + cn-1*c0
-        vector<int> dp(n+1, 0);
-        dp[0] = 1;
-        for(int i = 1; i <= n; i++)
-            for(int j = 0; j < i; j++)
-                dp[i] += dp[j] * dp[i-j-1];
-        return dp[n];
+        long c = 1;
+        for(int i = 1; i < n; i++) {
+            c = c*2*(2*i+1)/(i+2);
+        }
+        return c;
     }
 };
